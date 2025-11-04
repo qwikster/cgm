8dccbededca432c168b8c4dd73df30dfa5ed5e5c
qwikster <ocoincs@gmail.com>
2025-11-04 01:28:04 -0500

    scaffold for board draw, good night github

--- src/cgm/draw.py
+++ src/cgm/draw.py
@@ -1,3 +1,4 @@
+# DRAW.PY
 # Module for drawing to the screen (stdout tomfuckery time!!!)
 import sys
 import shutil
@@ -13,26 +14,25 @@ def validate_board(board): # check for dimensions in case game loop fucked me
     if widths != {10}:
         raise ValueError(f"Board has rows not 10 wide - {widths}")
 
-def get_cells(piece): # {"name": "i", "pos": (0, 0), "rotation": "0"}
-    # Gets the cells in a falling piece with its current rotation and coordinates on the board
-    shape = pieces[piece["name"]]["rotations"][piece["rotation"]]
-    px, py = piece["pos"]
-    cells = [(px + x, py + y)
-             for y, row in enumerate(shape)
-             for x, val in enumerate(row) if val]
-    return cells
+def format_time(ms: int) -> str: # for TGM style timer at the bottom
+    minutes = (ms // 1000) // 60 # Shame i can't make it obnoxiously big
+    seconds = (ms // 1000) % 60  # or can i...? *mice on venus*
+    hundredths = (ms % 1000) // 10
+    return f"{minutes:02}:{seconds:02}:{hundredths:02}"
 
-def get_color(piece_id: str | None): # ["i", "z", "s", "l", "j", "t", "o"]
-    if not piece_id:
+def color_block(piece_id: str | None): # in ["i", "z", "s", "l", "j", "t", "o"]
+    if (not piece_id) or (piece_id not in pieces):
         return EMPTY
     r, g, b = pieces[piece_id]["rgb"]
-    return f"\x1b[38;2;{r};{g};{b}m{BLOCK}{RESET}" # color, block ascii, reset
+    return f"\x1b[38;2;{r};{g};{b}m{BLOCK}\x1b[0m" # color, block ascii, reset
 
-def collides(piece, board):
-    # Check if the piece is colliding (will want to write a new one for srs.py)
-    for (x, y) in get_cells(piece):
-        if x < 0 or x >= len(board[0]) or y >= len(board):
-            return True
-        if y >= 0 and board[y][x][0]:
-            return True
-    return False
+# public
+def draw_board(board,                              # from game.py. Board: 2d list. Entries: [0] or [1, "t"]. 
+               active_piece = None,                # {"name": "t", "pos": (0, 0), "rotation": "0"}
+               score: int = 0, grade: str = "9",   # score is used to calculate TGM grade (9 -> 1 -> S1 -> S9 -> Gm)
+               time_ms: int = 0,                   # BIG FLASHING TIMER
+               lines: int = 0, line_goal: int = 0, # for gravity level up with timer
+               hold: str | None = None,            # hold = "t"
+               next_queue: list[str] | None = None # list of 5 pieces like ["t", "i", "z", "s", "o"] (7-bag)
+               ):
+    pass
--- src/cgm/game.py
+++ src/cgm/game.py
@@ -1,1 +1,20 @@
 # core logic like lock, gravity, and scoring
+from pieces import pieces
+
+def get_cells(piece): # {"name": "i", "pos": (0, 0), "rotation": "0"}
+    # Gets the cells in a falling piece with its current rotation and coordinates on the board
+    shape = pieces[piece["name"]]["rotations"][piece["rotation"]]
+    px, py = piece["pos"]
+    cells = [(px + x, py + y)
+             for y, row in enumerate(shape)
+             for x, val in enumerate(row) if val]
+    return cells
+
+def collides(piece, board):
+    # Check if the piece is colliding (will want to write a new one for srs.py)
+    for (x, y) in get_cells(piece):
+        if x < 0 or x >= len(board[0]) or y >= len(board):
+            return True
+        if y >= 0 and board[y][x][0]:
+            return True
+    return False
