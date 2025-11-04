59660efe2eb3866b3f6bec0e51892e833d77c550
qwikster <ocoincs@gmail.com>
2025-11-03 21:02:04 -0500

    draw to terminal, test they're right

--- src/cgm/main.py
+++ src/cgm/main.py
@@ -1,8 +1,23 @@
 # Main module
 import sys
+from pieces import pieces
+import time
 
 def entry():
-    print("bring back 69 the new gens ruined it")
+    while(1):
+        for rot in ["0", "r", "2", "l"]:
+            for k in pieces:
+                for i in pieces[k]["rotations"][rot]:
+                    r, g, b = pieces[k]["rgb"]
+                    print(f"\x1b[38;2;{r};{g};{b}m", end = "")
+                    for j in i:
+                        if j:
+                            print("██", end="")
+                        else:
+                            print("  ", end="")
+                    print()
+            time.sleep(0.3)
+            print("\033[2J\033[H")
 
 if __name__ == "__main__":
     try:
