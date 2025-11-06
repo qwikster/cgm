ebf4ad7c8ef7a686caae716bf9a91208fe14480b
qwikster <ocoincs@gmail.com>
2025-11-06 02:29:11 -0500

    bag system done, basic render loop on

---
+++ src/cgm/bag.py
@@ -0,0 +1,28 @@
+# 7-bag randomizer
+
+import random
+from tables import pieces
+
+PIECE_BAG = list(pieces.keys())
+
+class Bag:
+    def __init__(self, preview_size: int = 5):
+        self.preview_size = preview_size
+        self.current_bag = []
+        self.next_bag = []
+        self.fill_bag()
+    
+    def fill_bag(self):
+        if not self.next_bag:
+            self.next_bag = random.sample(PIECE_BAG, len(PIECE_BAG))
+    
+    def get_piece(self):
+        if not self.current_bag:
+            self.current_bag = self.next_bag
+            self.next_bag = []
+            self.fill_bag()
+        return self.current_bag.pop(0)
+    
+    def get_preview(self):
+        upcoming = self.current_bag + self.next_bag
+        return upcoming[:self.preview_size]
--- src/cgm/main.py
+++ src/cgm/main.py
@@ -3,48 +3,66 @@ import os
 import sys
 from tables import pieces, grades, thresholds
 from draw import draw_board
-from game import lock_piece, get_next
+from game import lock_piece
 from player import Player
+from bag import Bag
 import time
 import threading
-        
-def render_loop(board, player, next_queue):
-    draw_board(board, player.active_piece, )
+import signal
+
+sigint = threading.Event()
+
+def sigint_handler(sig, frame):
+    sigint.set()
+    print("goodbye!")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
 
 def setup_board(rows, cols):
     board = [[[0] for _ in range(cols)] for _ in range(rows)]
     return board
 
+def render_loop(board, player, bag, fps):
+    frame_time = 1.0 / fps
+    while not sigint.is_set():
+        start = time.perf_counter()
+        player.upd_time()
+        draw_board(
+            board,
+            player.active_piece,
+            player.score,
+            player.grade,
+            player.time_ms,
+            player.level,
+            player.line_goal,
+            player.hold_piece,
+            bag.get_preview()
+        ) # got damn
+        elapsed = time.perf_counter() - start
+        sleep_time = frame_time - elapsed
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        else:
+            raise TimeoutError("running under 60fps!")
+
 def entry():
     player = Player()
+    bag = Bag(preview_size=5)
     board = setup_board(22, 10)
-    next_queue = get_next(5)
-    
-    player.active_piece = {"name": "t", "pos": (3, 0), "rotation": "0"}
-    draw_board(board, player.active_piece, player.score, player.grade, player.time_ms, player.level, player.line_goal, player.hold_piece, next_queue)
-    input()
-    
-    render_thread = threading.Thread(target=render_loop, args=(board, player, next_queue))
-    render_thread.start()
-
-if __name__ == "__main__":
-    entry()
-    
     try:
-        # entry()
+        render_thread = threading.Thread(target=render_loop, args=(board, player, bag, 60))
+        render_thread.start()
+    except KeyboardInterrupt:
+        sigint.set()
+    
+    while(1):
+        player.active_piece = {"name": bag.get_piece(), "pos": (3, 0), "rotation": "0"}
+        input()
+
+if __name__ == "__main__":    
+    try:
         os.system("clear")
-        
-        start = time.perf_counter()
-        for _ in range(40):
-            for j in ["9", "8", "7", "6", "5", "4", "3", "2", "1", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "Gm"]:
-                piece = {"name": "t", "pos": (3, 20), "rotation": "0"}
-                board, lost = lock_piece(piece, board)
-                if lost:
-                    sys.exit(0)
-                piece = {"name": "t", "pos": (3, 0), "rotation": "0"}
-                draw_board(board, piece, 1696969, j, 480000, 123, 999, "t", ["j", "l", "s", "z", "o"])
-                time.sleep(0.3)
-        end = time.perf_counter()
-        print(f"Took {end - start} seconds")
+        entry()
     except KeyboardInterrupt:
         sys.exit(0)
